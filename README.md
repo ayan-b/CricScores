2cc01cfeab2ae137873ba20981e51a04e0b31667
ayan-b <ayanbanerjee7777@gmail.com>
2018-04-16 01:00:21 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#CricScores
+
+Simply run the <code>main.py</code> file to start the application. Put the link of the desired match in the <code>url</code> variable in the <code>main.py</code> file. The application will show the score every 5 minutes. You may change the interval by changing the <code>interval</code> variable in the <code>main</code> file.
---
+++ main.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen, urlretrieve
+from time import time, sleep, ctime
+from balloontip import balloon_tip
+
+starttime = time()
+interval = 300.0
+while True:
+    url = "http://www.espncricinfo.com/series/8048/game/1136572/kings-xi-punjab-vs-chennai-super-kings-12th-match-indian-premier-league-2018"
+    source_code = BeautifulSoup(urlopen(url).read(), "html.parser")
+    t = source_code.title.string
+    t = t.replace ("Match Summary","")
+    t = t.replace ("ESPNCricinfo","")
+    t = t.replace ("|","",2)
+    print (ctime(), t)
+    balloon_tip("CricScore", t)
+    sleep(interval - ((time() - starttime) % interval))
