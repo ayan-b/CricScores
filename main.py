943c0921a7273ee3a590a6333f7ae10b72b7814a
ayan-b <ayanbanerjee7777@gmail.com>
2018-06-14 14:05:33 +0530

    PEP8 fixes

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ from balloontip import balloon_tip
 
 starttime = time()
 interval = 300.0
+
+
 while True:
-    url = "http://www.espncricinfo.com/series/8048/game/1136572/kings-xi-punjab-vs-chennai-super-kings-12th-match-indian-premier-league-2018"
+    url = "http://www.espncricinfo.com/series/8048/game/1136572/kings-xi-" +
+    "punjab-vs-chennai-super-kings-12th-match-indian-premier-league-2018"
     source_code = BeautifulSoup(urlopen(url).read(), "html.parser")
     t = source_code.title.string
-    t = t.replace ("Match Summary","")
-    t = t.replace ("ESPNCricinfo","")
-    t = t.replace ("|","",2)
-    print (ctime(), t)
+    t = t.replace("Match Summary", "")
+    t = t.replace("ESPNCricinfo", "")
+    t = t.replace("|", "", 2)
+    print(ctime(), t)
     balloon_tip("CricScore", t)
     sleep(interval - ((time() - starttime) % interval))
